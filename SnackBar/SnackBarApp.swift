5e2629ec1229c5396c163ade2950c28a3a73a271
Sraavan Chevireddy <schevireddy@saventech.com>
2022-08-01 21:07:12 +0530

    Initial Commit

---
+++ SnackBar/SnackBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackBarApp.swift
+//  SnackBar
+//
+//  Created by Sraavan Chevireddy on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SnackBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
